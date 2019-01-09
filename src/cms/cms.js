d7c63e5d2893bd7dca626e22f8a6cb6c183a6ed3
Vo Van Trung <trungvv0511@gmail.com>
2019-01-09 20:57:34 +0700

    test youtube editor

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -13,3 +13,24 @@ CMS.registerPreviewTemplate("navbar", NavbarPreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("home", HomePagePreview);
 CMS.registerPreviewTemplate("pastMeetups", PastMeetupsPagePreview);
+
+CMS.registerEditorComponent({
+    id: "youtube",
+    label: "Youtube",
+    icon: 'video',
+    fields: [{name: 'id', label: 'Youtube Video ID'}],
+    pattern: /^{{<\s?youtube (\S+)\s?>}}/,
+    fromBlock: function(match) {
+      return {
+        id: match[1]
+      };
+    },
+    toBlock: function(obj) {
+      return '{{< youtube ' + obj.id + ' >}}';
+    },
+    toPreview: function(obj) {
+      return (
+        '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+      );
+    }
+  });
